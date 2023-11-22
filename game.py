07e484591e97af5e46a5b50883723a3400d73e85
jcyalung <jyalung5@student.ohlone.edu>
2023-11-22 11:42:40 -0800

    added game.py for organization of cards

---
+++ game.py
@@ -0,0 +1,10 @@
+card_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven","Eight","Nine","Ten","Jack","Queen", "King"]
+card_suits = ["Spades","Diamonds","Clubs","Hearts"]
+
+def deck():
+    card_deck = []
+    for i in range(0, len(card_suits)):
+        for j in range(0, len(card_values)):
+            card_deck.append(card_values[j] + " of " + card_suits[i])
+    return(card_deck)
+
--- server.py
+++ server.py
@@ -1,20 +1,11 @@
 from fastapi import FastAPI
+import game
 import uvicorn
 import random
 
 app = FastAPI()
 
-card_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven","Eight","Nine","Ten","Jack","Queen", "King"]
-card_suits = ["Spades","Diamonds","Clubs","Hearts"]
 
-def deck(card_values, card_suits):
-    card_deck = []
-    for i in range(0, len(card_suits)):
-        for j in range(0, len(card_values)):
-            card_deck.append(card_values[j] + " of " + card_suits[i])
-    return(card_deck)
-
-card_deck = deck(card_values, card_suits)
 
 @app.get("/")
 def root():
@@ -48,17 +39,20 @@ def flip_coins(times: int = 10):
 
 @app.get("/draw-card")
 def draw_card():
-    card_deck = deck(card_values, card_suits)
+    card_deck = game.deck()
     val = random.uniform(0, 51)
     drawn = card_deck[int(val)]
     return({"Card" : drawn})
 
 @app.get("/draw-cards")
 def draw_cards(times: int = 5):
+    card_deck = game.deck()
+    if(times > 52):
+        return ({ "message": "you cannot draw more than 52 cards" })
     if times and times > 0:
         drawn = {}
         for i in range(times):
-            val = random.uniform(0,52)
+            val = random.uniform(0,51)
             drawn[card_deck[int(val)]] = 1
         return(drawn)
     else:
